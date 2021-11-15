80ebe19cb6bbf117eb1761eb14dd9635875d39bf
Luis Boscan <luis.boscan@globant.com>
2021-11-15 12:45:13 -0300

    Build & push image added

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
   echo "Waiting for postgres server"
   sleep 1
 done
